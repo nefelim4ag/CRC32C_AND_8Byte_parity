992db9bcc1445e1a492bfbeff5c7557295116c0d
Timofey Titovets <nefelim4ag@gmail.com>
2017-09-20 03:19:55 +0300

    Update
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- 8byte_parity.c
+++ 8byte_parity.c
@@ -8,7 +8,7 @@
 #include "xxhash.h"
 #include "crc32.h"
 
-uint32_t parity_32(uint8_t *data, uint64_t byte_len, uint32_t seed) {
+static uint32_t parity_32(uint8_t *data, uint64_t byte_len, uint32_t seed) {
         uint32_t parity = seed;
         uint32_t step = sizeof(parity);
         uint32_t *ptr = (uint32_t *) data;
@@ -21,7 +21,7 @@ uint32_t parity_32(uint8_t *data, uint64_t byte_len, uint32_t seed) {
         return parity;
 }
 
-uint64_t parity_64(const void *data, uint64_t byte_len, uint64_t seed) {
+static uint64_t parity_64(const void *data, uint64_t byte_len, uint64_t seed) {
         uint64_t parity = seed;
         uint64_t *ptr = (uint64_t *) data;
 
@@ -36,67 +36,67 @@ uint64_t parity_64(const void *data, uint64_t byte_len, uint64_t seed) {
         return parity;
 }
 
+//int parity_fix_error
+
 int main() {
         uint8_t PAGE[4*1024];
         uint64_t i;
-        uint32_t parity32;
-        uint64_t parity64;
-        uint64_t hash64;
-        uint32_t crc = 0;
         uint64_t iter = 1024;
-        srand(time(NULL));
         clock_t start, end;
+        srand(time(NULL));
+
 
         iter *= 1024*1024*4/sizeof(PAGE);
 
         for (uint32_t i = 0; i < sizeof(PAGE); i++)
                 PAGE[i] = rand()%256;
 
+        printf("--- Test speed of hash/parity functions ---\n");
+
         printf("PAGE_SIZE: %lu, loop count: %lu\n", sizeof(PAGE), iter);
 
-        start = clock()*1000000/CLOCKS_PER_SEC;
-        for (i = 0; i < iter; i++) {
-                parity64 = parity_64((uint8_t *) &PAGE, sizeof(PAGE), 0xdeadc0dedeadc0de);
+        {
+                uint64_t parity64;
+                start = clock()*1000000/CLOCKS_PER_SEC;
+                for (i = 0; i < iter; i++) { parity64 = parity_64((uint8_t *) &PAGE, sizeof(PAGE), 0); }
+                end = clock()*1000000/CLOCKS_PER_SEC;
+                printf("Parity64:\t0x%" PRIx64 "\t\t\tperf: %lu µs,\tth: %f MiB/s\n", parity64, (end - start), sizeof(PAGE)*iter*1.0/(end - start));
         }
-        end = clock()*1000000/CLOCKS_PER_SEC;
-
-        printf("Parity64:\t0x%" PRIx64 "\t\t\tperf: %lu ms,\tth: %f MiB/s\n", parity64, (end - start)/1000, sizeof(PAGE)*iter*1.0/(end - start));
-
-        start = clock()*1000000/CLOCKS_PER_SEC;
-        for (i = 0; i < iter; i++) {
-                parity32 = parity_32((uint8_t *) &PAGE, sizeof(PAGE), 0xdeadcafe);
-        }
-        end = clock()*1000000/CLOCKS_PER_SEC;
-
-        printf("parity32:\t0x%" PRIx32 "\t\t\t\tperf: %lu ms,\tth: %f MiB/s\n", parity32, (end - start)/1000, sizeof(PAGE)*iter*1.0/(end - start));
-
-
-        start = clock()*1000000/CLOCKS_PER_SEC;
-        for (i = 0; i < iter; i++) {
-                crc = crc32c(0, &PAGE, sizeof(PAGE));
-        }
-        end = clock()*1000000/CLOCKS_PER_SEC;
-
-        printf("crc32:\t\t0x%" PRIx32 "\t\t\t\tperf: %lu ms,\tth: %f MiB/s\n", crc, (end - start)/1000, sizeof(PAGE)*iter*1.0/(end - start));
-
-        start = clock()*1000000/CLOCKS_PER_SEC;
-        for (i = 0; i < iter; i++) {
-                hash64 = xxh64(&PAGE, sizeof(PAGE), 0xdeadc0dedeadc0de);
-        }
-        end = clock()*1000000/CLOCKS_PER_SEC;
-
-        printf("xxhash64:\t0x%" PRIx64 "\t\t\tperf: %lu ms,\tth: %f MiB/s\n", hash64, (end - start)/1000, sizeof(PAGE)*iter*1.0/(end - start));
-
-
-        /* Try add error and fix it */
 
         {
-                uint64_t orig_seed = 0x0;
+                uint32_t parity32;
+                start = clock()*1000000/CLOCKS_PER_SEC;
+                for (i = 0; i < iter; i++) { parity32 = parity_32((uint8_t *) &PAGE, sizeof(PAGE), 0); }
+                end = clock()*1000000/CLOCKS_PER_SEC;
+                printf("parity32:\t0x%" PRIx32 "\t\t\t\tperf: %lu µs,\tth: %f MiB/s\n", parity32, (end - start), sizeof(PAGE)*iter*1.0/(end - start));
+        }
+
+        {
+                uint32_t crc;
+                start = clock()*1000000/CLOCKS_PER_SEC;
+                for (i = 0; i < iter; i++) { crc = crc32c(0, &PAGE, sizeof(PAGE)); }
+                end = clock()*1000000/CLOCKS_PER_SEC;
+                printf("crc32:\t\t0x%" PRIx32 "\t\t\t\tperf: %lu µs,\tth: %f MiB/s\n", crc, (end - start), sizeof(PAGE)*iter*1.0/(end - start));
+        }
+
+        {
+                uint64_t hash64;
+                start = clock()*1000000/CLOCKS_PER_SEC;
+                for (i = 0; i < iter; i++) { hash64 = xxh64(&PAGE, sizeof(PAGE), 0); }
+                end = clock()*1000000/CLOCKS_PER_SEC;
+                printf("xxhash64:\t0x%" PRIx64 "\t\t\tperf: %lu µs,\tth: %f MiB/s\n", hash64, (end - start), sizeof(PAGE)*iter*1.0/(end - start));
+        }
+
+        /* Try add error and fix it */
+        printf("--- Example of error injection and fixup ---\n");
+
+        {
+                uint64_t orig_seed = 0xbadc3cc0de;
                 uint64_t orig_parity = parity_64((uint8_t *) &PAGE, sizeof(PAGE), orig_seed);
                 uint64_t orig_xxhash64 = xxh64(&PAGE, sizeof(PAGE), orig_seed);
                 uint32_t orig_crc = crc32c(0, &PAGE, sizeof(PAGE));
-                uint64_t PAGE_FIXED[sizeof(PAGE)/sizeof(orig_parity)];
                 uint64_t stripe_num = sizeof(PAGE)/sizeof(orig_parity);
+                uint64_t PAGE_FIXED[stripe_num];
 
                 memcpy(&PAGE_FIXED, &PAGE, sizeof(PAGE));
 
@@ -104,7 +104,8 @@ int main() {
 
                 PAGE[rand()%sizeof(PAGE)] = rand()%256;
 
-                // Brute force broken part
+                /* Brute force broken part */
+                start = clock()*1000000/CLOCKS_PER_SEC;
                 for (i = 0; i < stripe_num; i++) {
                         memcpy(&PAGE_FIXED, &PAGE, sizeof(PAGE));
                         PAGE_FIXED[i] = orig_parity;
@@ -117,6 +118,9 @@ int main() {
                                 break;
                         }
                 }
+                end = clock()*1000000/CLOCKS_PER_SEC;
+                printf("perf: %lu µs,\tth: %f MiB/s\n", (end - start), sizeof(PAGE)*i*1.0/(end - start));
+
                 uint64_t new_xxhash64 = xxh64(&PAGE_FIXED, sizeof(PAGE_FIXED), orig_seed);
                 if (new_xxhash64 == orig_xxhash64) {
                         printf("xxhash64: matched!\n");
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # CRC32C_AND_8Byte_parity
 Proof of concept 8Byte Parity + CRC32C as checksum
+
+```
+~$ make clean; make; ./8byte_parity
+rm -fv *.o
+removed '8byte_parity.o'
+removed 'crc32.o'
+removed 'xxhash.o'
+cc -O2 -Wall -Werror -pthread -c 8byte_parity.c -o 8byte_parity.o
+cc -O2 -Wall -Werror -pthread -c xxhash.c -o xxhash.o
+cc -O2 -Wall -Werror -pthread -c crc32.c -o crc32.o
+cc -O2 -Wall -Werror -pthread -o 8byte_parity 8byte_parity.o xxhash.o crc32.o
+--- Test speed of hash/parity functions ---
+PAGE_SIZE: 4096, loop count: 1048576
+Parity64:       0x73844fb949a04d18                      perf: 241981 µs,        th: 17749.192275 MiB/s
+parity32:       0x3a2402a1                              perf: 453929 µs,        th: 9461.760090 MiB/s
+crc32:          0xcc5a1265                              perf: 311659 µs,        th: 13780.982728 MiB/s
+xxhash64:       0x76d03edf67d0d854                      perf: 360420 µs,        th: 11916.562055 MiB/s
+--- Example of error injection and fixup ---
+Stripe num: 512
+Error found at: 0xb28
+CRC32C: Match!
+Probably fixed...
+perf: 210 µs,   th: 6963.200000 MiB/s
+xxhash64: matched!
+Error was fixed!
+```
